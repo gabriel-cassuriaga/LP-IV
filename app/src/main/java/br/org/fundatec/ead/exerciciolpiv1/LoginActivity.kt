4a566865bff39a12b52f1c45a0d539a45b268c5f
João Gabriel C <gabriel_cassuriaga@hotmail.com>
2023-04-02 00:12:51 -0300

    Splash-Login

---
+++ app/src/main/java/br/org/fundatec/ead/exerciciolpiv1/LoginActivity.kt
@@ -0,0 +1,26 @@
+package br.org.fundatec.ead.exerciciolpiv1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val verMainButton = findViewById<Button>(R.id.login)
+        verMainButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val verPerfilView = findViewById<View>(R.id.novo_aqui)
+        verPerfilView.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/br/org/fundatec/ead/exerciciolpiv1/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package br.org.fundatec.ead.exerciciolpiv1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
